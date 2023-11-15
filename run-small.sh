8ed59988b49b31315b71d7b004767ecfa462ce60
ALVARO CABRERA BARRIO <a0432481@avignon-frontend.lab.inf.uc3m.es>
2023-11-15 15:35:48 +0100

    Unos cuantos tests mas

--- run-small.sh
+++ run-small.sh
@@ -4,8 +4,11 @@ perf stat build/fluid/fluid 250 res/small.fld res/small-temp.fld
 perf stat build/fluid/fluid 500 res/small.fld res/small-temp.fld
 perf stat build/fluid/fluid 750 res/small.fld res/small-temp.fld
 perf stat build/fluid/fluid 1000 res/small.fld res/small-temp.fld
+perf stat build/fluid/fluid 1250 res/small.fld res/small-temp.fld
 perf stat build/fluid/fluid 1500  res/small.fld res/small-temp.fld
+perf stat build/fluid/fluid 1750  res/small.fld res/small-temp.fld
 perf stat build/fluid/fluid 2000 res/small.fld res/small-temp.fld
+perf stat build/fluid/fluid 2250 res/small.fld res/small-temp.fld
 perf stat build/fluid/fluid 2500 res/small.fld res/small-temp.fld
 
 
@@ -16,6 +19,9 @@ perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/flu
 perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/fluid 500 res/small.fld res/small-temp.fld
 perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/fluid 750 res/small.fld res/small-temp.fld
 perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/fluid 1000 res/small.fld res/small-temp.fld
+perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/fluid 1250 res/small.fld res/small-temp.fld
 perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/fluid 1500 res/small.fld res/small-temp.fld
+perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/fluid 1750 res/small.fld res/small-temp.fld
 perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/fluid 2000 res/small.fld res/small-temp.fld
+perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/fluid 2250 res/small.fld res/small-temp.fld
 perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/fluid 2500 res/small.fld res/small-temp.fld
--- run.sh
+++ run.sh
@@ -4,8 +4,11 @@ perf stat build/fluid/fluid 250 res/large.fld res/large-temp.fld
 perf stat build/fluid/fluid 500 res/large.fld res/large-temp.fld
 perf stat build/fluid/fluid 750 res/large.fld res/large-temp.fld
 perf stat build/fluid/fluid 1000 res/large.fld res/large-temp.fld
+perf stat build/fluid/fluid 1250 res/large.fld res/large-temp.fld
 perf stat build/fluid/fluid 1500  res/large.fld res/large-temp.fld
+perf stat build/fluid/fluid 1750  res/large.fld res/large-temp.fld
 perf stat build/fluid/fluid 2000 res/large.fld res/large-temp.fld
+perf stat build/fluid/fluid 2250 res/large.fld res/large-temp.fld
 perf stat build/fluid/fluid 2500 res/large.fld res/large-temp.fld
 
 
@@ -16,6 +19,9 @@ perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/flu
 perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/fluid 500 res/large.fld res/large-temp.fld
 perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/fluid 750 res/large.fld res/large-temp.fld
 perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/fluid 1000 res/large.fld res/large-temp.fld
+perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/fluid 1250 res/large.fld res/large-temp.fld
 perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/fluid 1500 res/large.fld res/large-temp.fld
+perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/fluid 1750 res/large.fld res/large-temp.fld
 perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/fluid 2000 res/large.fld res/large-temp.fld
+perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/fluid 2250 res/large.fld res/large-temp.fld
 perf stat -a -e 'energy-cores, energy-gpu,energy-pkg,energy-ram' build/fluid/fluid 2500 res/large.fld res/large-temp.fld
