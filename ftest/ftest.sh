23947151129efb07d9e5748f3cbf76a80650afb0
ALVARO CABRERA BARRIO <a0432481@avignon-frontend.lab.inf.uc3m.es>
2023-11-22 12:30:41 +0100

    Test funcional

--- ftest/ftest.sh
+++ ftest/ftest.sh
@@ -1,28 +1,21 @@
 #!/bin/sh
 
-#Test funcional del programa en caso de que algun output no coincida con los outputs de referencia
-#proporcionados, este script mostrar por pantalla en cuantos bytes y donde difieren los outputs
 
-build/fluid/fluid 1 res/large.fld outs/results-large-1.fld
-build/fluid/fluid 2 res/large.fld outs/results-large-2.fld
-build/fluid/fluid 3 res/large.fld outs/results-large-3.fld
-build/fluid/fluid 4 res/large.fld outs/results-large-4.fld
-build/fluid/fluid 5 res/large.fld outs/results-large-5.fld
+#Test funcional, ejecuta de  1 a 5 iteraciones tanto para la entrada small como large y las compara con los outputs de referencia
+#proporcionados por el profesorado. Si son correctos se muestra por pantalla un mensaje de acierto, en caso contrario el propio cmp
+#indicara por pantalla que hubo una discrepancia entre ambos archivos.
+i=1
+while [ "$i" -lt 6 ]; do
 
-build/fluid/fluid 1 res/small.fld outs/results-small-1.fld
-build/fluid/fluid 2 res/small.fld outs/results-small-2.fld
-build/fluid/fluid 3 res/small.fld outs/results-small-3.fld
-build/fluid/fluid 4 res/small.fld outs/results-small-4.fld
-build/fluid/fluid 5 res/small.fld outs/results-small-5.fld
+	build/fluid/fluid $i res/large.fld "outs/results-large-${i}.fld" > /dev/null 2>&1
+	if cmp "outs/results-large-${i}.fld" "res/out/large-${i}.fld"; then
+		echo "Resultados para large con ${i} iteraciones correctos"
+	fi
 
-cmp outs/results-large-1.fld res/out/large-1.fld
-cmp outs/results-large-2.fld res/out/large-2.fld
-cmp outs/results-large-3.fld res/out/large-3.fld
-cmp outs/results-large-4.fld res/out/large-4.fld
-cmp outs/results-large-5.fld res/out/large-5.fld
+	build/fluid/fluid $i res/small.fld "outs/results-small-${i}.fld" > /dev/null 2>&1
+	if cmp "outs/results-small-${i}.fld" "res/out/small-${i}.fld"; then
+		echo "Resultados para small con ${i} iteraciones correctos"
+	fi
+	i=$((i+1))
+done
 
-cmp outs/results-small-1.fld res/out/small-1.fld
-cmp outs/results-small-2.fld res/out/small-2.fld
-cmp outs/results-small-3.fld res/out/small-3.fld
-cmp outs/results-small-4.fld res/out/small-4.fld
-cmp outs/results-small-5.fld res/out/small-5.fld
